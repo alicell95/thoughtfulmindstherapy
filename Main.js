0819504a312fe4a40a8601f3cbe2c65c789ad439
alicell95 <149379276+alicell95@users.noreply.github.com>
2024-05-30 14:50:35 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -10,6 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
     primaryNav.setAttribute('data-visible', !isVisible);
     navToggle.setAttribute('aria-expanded', !isVisible);
     primaryHeader.setAttribute('data-overlay', !isVisible);
+
+    if (!isVisible) {
+      navToggle.querySelector('.icon-open').style.display = 'none';
+      navToggle.querySelector('.icon-close').style.display = 'block';
+    } else {
+      navToggle.querySelector('.icon-open').style.display = 'block';
+      navToggle.querySelector('.icon-close').style.display = 'none';
+    }
   });
 });
 
